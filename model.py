b35201928e50aeb9aba0360325c84e79b399ebcf
FelixL0120 <xx_lai@sjtu.edu.cn>
2025-01-05 20:17:00 +0800

    Update model.py

--- model.py
+++ model.py
@@ -1,5 +1,17 @@
+# /model.py
+
+class ItemType:
+    def __init__(self, name, attributes=None):
+        self.name = name
+        self.attributes = attributes if attributes else {}
+
 class Item:
-    def __init__(self, name, description, contact):
+    def __init__(self, item_type, name, description, address, contact_phone, email, **kwargs):
+        self.item_type = item_type
         self.name = name
         self.description = description
-        self.contact = contact
+        self.address = address
+        self.contact_phone = contact_phone
+        self.email = email
+        for key, value in kwargs.items():
+            setattr(self, key, value)
