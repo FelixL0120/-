4c938a716e36145b08a8599dca4691d0dfe05c04
FelixL0120 <xx_lai@sjtu.edu.cn>
2025-01-05 20:17:47 +0800

    Update data.py

--- data.py
+++ data.py
@@ -1,35 +1,46 @@
+# /data.py
+
 import json
+from model import ItemType, Item
 
 DATA_FILE = 'items.json'
+USER_DATA_FILE = 'users.json'
 
-def load_items():
+def load_data():
     try:
         with open(DATA_FILE, 'r') as file:
             return json.load(file)
     except FileNotFoundError:
         return {}
 
-def save_items(items):
+def save_data(items):
     with open(DATA_FILE, 'w') as file:
         json.dump(items, file, indent=4)
 
-def add_item(items, name, description, contact):
-    if name in items:
-        return False, "物品已存在。"
-    items[name] = {
-        'description': description,
-        'contact': contact
-    }
-    return True, "物品添加成功。"
+def load_users():
+    try:
+        with open(USER_DATA_FILE, 'r') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return {}
 
-def remove_item(items, name):
-    if name in items:
-        del items[name]
-        return True, "物品删除成功。"
-    return False, "物品不存在。"
+def save_users(users):
+    with open(USER_DATA_FILE, 'w') as file:
+        json.dump(users, file, indent=4)
 
-def find_item(items, name):
-    if name in items:
-        info = items[name]
-        return True, f"{name}: {info['description']}, 联系人: {info['contact']}"
-    return False, "物品不存在。"
+# 我们有以下物品类型数据结构
+item_types = {
+    "food": ItemType("food", {"expiration_date": "", "quantity": ""}),
+    "book": ItemType("book", {"author": "", "publisher": ""})
+}
+
+def get_item_types():
+    return item_types
+
+def add_item_type(item_type):
+    item_types[item_type.name] = item_type
+
+def add_item(item):
+    items = load_data()
+    items[item.name] = item.__dict__
+    save_data(items)
