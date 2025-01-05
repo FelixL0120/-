2d67d20ade7ddd4a07ba16468ac9244877d4105e
FelixL0120 <xx_lai@sjtu.edu.cn>
2025-01-05 21:32:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-from gui import create_gui
+from gui import create_gui, create_register_gui, create_admin_approve_gui
 
 def main():
-    create_gui()
+    import sys
+    if len(sys.argv) > 1 and sys.argv[1] == 'register':
+        create_register_gui()
+    elif len(sys.argv) > 1 and sys.argv[1] == 'admin':
+        create_admin_approve_gui()
+    else:
+        create_gui()
 
 if __name__ == "__main__":
     main()
